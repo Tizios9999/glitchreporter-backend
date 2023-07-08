cc33d4207d848e5f47abab883e5f981a4e847233
Tizios9999 <davide.santonocito@outlook.com>
2023-07-08 13:28:07 +0200

    Added DTO package

---
+++ src/main/java/com/ds/glitchreporter/dto/MessageDTO.java
@@ -0,0 +1,13 @@
+package com.ds.glitchreporter.dto;
+
+import java.time.LocalDate;
+
+public class MessageDTO {
+	
+	private Long ticketId;
+	private String senderId;
+	private String sender;
+	private String message;
+	private LocalDate date;
+	
+}
