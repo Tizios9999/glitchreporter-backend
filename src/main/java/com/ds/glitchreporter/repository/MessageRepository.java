762914f1aa22d77089bd7e67fef4e52e401f5c07
Tizios9999 <davide.santonocito@outlook.com>
2023-07-09 09:52:46 +0200

    added findByTicketId method

--- src/main/java/com/ds/glitchreporter/repository/MessageRepository.java
+++ src/main/java/com/ds/glitchreporter/repository/MessageRepository.java
@@ -1,5 +1,7 @@
 package com.ds.glitchreporter.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.ds.glitchreporter.models.Message;
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
+	List<Message> findByTicketId(Long ticketId);
 }
